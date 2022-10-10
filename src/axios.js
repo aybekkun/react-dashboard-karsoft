da9ee66afdde37c7dd4277d311703af8646b48a8
Aybek Amanbaev <idevkun@gmail.com>
2022-10-10 21:45:20 +0500

    change http

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://rivojyulduz.karsoft.uz/api",
+  baseURL: "https://rivojyulduz.karsoft.uz/api",
 });
 
 instance.interceptors.request.use((config) => {
